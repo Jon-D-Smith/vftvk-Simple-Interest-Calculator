898d0258f56c7886867f8372fa47a7d752d7dd52
Jon Smith <aardvarkjon94@yahoo.com>
2021-02-25 11:15:33 -0600

    Setup HTML and CSS Structure; Added some JS functionality

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ function compute()
     p = document.getElementById("principal").value;
     
 }
-        
+ 
+
+function getRangeValue(){
+    let rate = document.getElementById("rate").value;
+    let outPut = document.getElementById("rateValue");
+
+    return outPut.innerHTML = (rate + "%");
+}
+

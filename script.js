3d527d37aeaab8c69ac11ccbc648c7d51b82b8f4
Jon Smith <aardvarkjon94@yahoo.com>
2021-02-26 15:08:32 -0600

    Finished functionality per program specs

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+
+const this_year = new Date().getFullYear();    
+const solution = document.getElementById('solution');
+const p = document.getElementById("principal").value;
+const rate = document.getElementById("rate").value;
+const years = document.getElementById("years").value;
+const equation = p * (1 + ((rate/100)*years))
+
+let answer = equation - p;
+
+
+   solution.innerHTML = "<span class='answers'> If you deposit " + p +   "<br/>" 
+   solution.innerHTML += "at an interest rate of " + rate +   "<br/>"                           
+   solution.innerHTML += "You will receive an amount of " + answer +   "<br/>" 
+   solution.innerHTML += "in the year " + (this_year + parseInt(years)) + "</span>"
+   console.log(this_year + parseInt(years));
+   console.log("Hi")
+   return solution.innerHtml
 }
  
 
